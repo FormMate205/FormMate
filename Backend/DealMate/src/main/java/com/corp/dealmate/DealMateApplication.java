95400eb2546312c8d06f8c69390baaf40d1eac48
yuncof <yun090710@gmail.com>
2025-03-11 12:39:27 +0900

    chore: 프로젝트 초기 설정

---
+++ Backend/DealMate/src/main/java/com/corp/dealmate/DealMateApplication.java
@@ -0,0 +1,13 @@
+package com.corp.dealmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealMateApplication.class, args);
+	}
+
+}
---
+++ Backend/DealMate/src/test/java/com/corp/dealmate/DealMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corp.dealmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
