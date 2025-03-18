a55faa8a141d0a8034a95c598394a00a9fc7d752
yiyoung yoon <2020yyoung@gmail.com>
2025-03-18 18:04:30 +0900

    publish: WIP 거래내역 페이지 컴포넌트 퍼블리싱

--- Frontend/src/pages/transaction/Transaction.tsx
+++ Frontend/src/pages/transaction/Transaction.tsx
@@ -1,40 +1,43 @@
+import { Button } from '../../components/ui/button';
 import { Icons } from '../../shared';
 
 const Transaction = () => {
     return (
         <>
-            <div className='bg-primary-50 px-4'>
-                <div className='flex flex-col gap-4'>
-                    <div className='flex items-center gap-2'>
-                        <span className='text-line-500 text-sm font-medium'>
-                            싸피뱅크 111-1111-1111
-                        </span>
-                        <Icons name='copy' size={18} />
+            <section className='bg-primary-50 px-4 py-8'>
+                <div className='flex flex-col gap-7'>
+                    <div className='flex flex-col gap-2'>
+                        <div className='flex items-center gap-2'>
+                            <span className='text-line-500 font-medium'>
+                                싸피뱅크 111-1111-1111
+                            </span>
+                            <Icons name='copy' size={20} />
+                        </div>
+                        <div className='text-3xl font-semibold'>12,345 원</div>
                     </div>
-                    <div className='text-3xl font-semibold'>12,345 원</div>
-                    <div>버튼</div>
+                    <Button variant='primary'>이체하기</Button>
                 </div>
-            </div>
-            <div className='p-4'>
+            </section>
+            <section className='p-4'>
                 <div className='flex flex-col gap-2'>
-                    <div className='text-line-700 border-line-200 border-b pb-1 text-sm font-medium'>
+                    <div className='text-line-700 border-line-200 border-b pb-1 font-medium'>
                         2025.03.12
                     </div>
                     <div className='border-line-100 flex justify-between border-b pb-2'>
-                        <div className='flex items-center font-medium'>
+                        <div className='flex items-center text-lg font-semibold'>
                             강지은
                         </div>
                         <div className='flex flex-col items-end'>
-                            <span className='text-line-700 text-sm font-medium'>
+                            <span className='text-line-700 font-medium'>
                                 출금
                             </span>
-                            <span className='text-primary-500 text-sm font-semibold'>
+                            <span className='text-primary-500 font-semibold'>
                                 -10,000원
                             </span>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </>
     );
 };
