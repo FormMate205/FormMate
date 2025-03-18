fb31ca063b0156335114c6751911afa2e85e3f01
yiyoung yoon <2020yyoung@gmail.com>
2025-03-18 15:29:15 +0900

    chore: 아이콘 sprite 이미지로 변환 및 세팅

--- Frontend/src/pages/form/FormCreate.tsx
+++ Frontend/src/pages/form/FormCreate.tsx
@@ -1,5 +1,9 @@
+import { Icons } from "../../shared";
+
 const FormCreate = () => {
-  return <div className="text-primary-500 font-bold">메인페이지</div>
+  return <div className="text-primary-500 font-bold">메인페이지
+  <Icons name="chat" size={24} className="fill-primary-300"/>
+  </div>
 }
 
 export default FormCreate;
