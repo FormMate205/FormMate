772676d97f32e825d819d8108dadcb88079fe2ed
yiyoung yoon <2020yyoung@gmail.com>
2025-03-18 15:41:16 +0900

    publish: 폴더명 변경

--- Frontend/src/pages/form/FormCreate.tsx
+++ Frontend/src/pages/form/FormCreate.tsx
@@ -2,7 +2,7 @@ import { Icons } from "../../shared";
 
 const FormCreate = () => {
   return <div className="text-primary-500 font-bold">메인페이지
-  <Icons name="chat" size={24} className="fill-primary-300"/>
+  <Icons name="chat" size={48} className="fill-primary-300"/>
   </div>
 }
 
--- Frontend/src/shared/index.tsx
+++ Frontend/src/shared/index.tsx
@@ -1,1 +1,1 @@
-export {default as Icons} from './icon/Icons'
+export {default as Icons} from './icons/Icons'
