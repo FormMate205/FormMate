1674f8d6f1caf25a0c77930991a4cde685870908
antis <rkdwldms42@naver.com>
2025-03-21 16:57:33 +0900

    publish: 챗봇페이지 초안 퍼블리싱

---
+++ Frontend/src/pages/form/FormChatBot.tsx
@@ -0,0 +1,17 @@
+import { Header } from '@/widgets';
+
+const FormChatBot = () => {
+    return (
+        <div className='flex h-screen flex-col items-center'>
+            <Header title='챗봇' />
+
+            {/* 채팅 내용 */}
+            <div></div>
+
+            {/* 채팅 입력창 */}
+            <div></div>
+        </div>
+    );
+};
+
+export default FormChatBot;
---
+++ Frontend/src/pages/form/index.tsx
@@ -0,0 +1,3 @@
+export { default as SearchListItem } from './ui/SearchListItem';
+export { default as ChatBox } from './ui/ChatBox';
+export { default as FormChatBot } from './FormChatBot';
