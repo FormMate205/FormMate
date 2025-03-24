2af45a609547dc952ee69d128a51ad361e84483a
oille12 <oeg9176@naver.com>
2025-03-24 16:18:27 +0900

    fix: React import 제거

--- Frontend/src/pages/home/ui/Calendar.tsx
+++ Frontend/src/pages/home/ui/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const Calendar = () => {
     const [weekDates, setWeekDates] = useState<Date[]>([]);
