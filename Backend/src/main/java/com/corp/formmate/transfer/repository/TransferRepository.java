d7c4ec4bd3468922606786543cbe812aad87832f
2Ludy <dkdl8412@gmail.com>
2025-03-27 17:43:46 +0900

    refactor: import 구문 변경

--- Backend/src/main/java/com/corp/formmate/transfer/repository/TransferRepository.java
+++ Backend/src/main/java/com/corp/formmate/transfer/repository/TransferRepository.java
@@ -1,12 +1,15 @@
 package com.corp.formmate.transfer.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.corp.formmate.form.entity.FormEntity;
 import com.corp.formmate.transfer.entity.TransferEntity;
 import com.corp.formmate.user.entity.UserEntity;
 
