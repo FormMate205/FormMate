73d448f4ee87b9f4ab1d8386374802e57d168331
antis <rkdwldms42@naver.com>
2025-04-02 17:42:17 +0900

    feat: 백엔드와 웹소켓 연결 중

--- Frontend/src/entities/chat/api/chatAPI.tsx
+++
@@ -1,6 +0,0 @@
-import api from '@/shared/api/instance';
-
-export const getMessages = async (roomId: string) => {
-    const response = await api.get(`/chat/${roomId}/messages`);
-    return response.data;
-};
