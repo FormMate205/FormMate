c46f7ae5be0ad8e37ca34cda47e81def1206954c
oille12 <oeg9176@naver.com>
2025-04-03 17:37:50 +0900

    refactor: Suspense 처리로 수정

--- Frontend/src/entities/home/model/useContractAmount.ts
+++ Frontend/src/entities/home/model/useContractAmount.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { getContractAmount } from '../api/getContractAmount';
+import { ContractAmountResponse } from './types';
 
 export const useContractAmount = () => {
-    return useQuery({
+    return useQuery<ContractAmountResponse>({
         queryKey: ['contractAmount'],
         queryFn: getContractAmount,
     });
