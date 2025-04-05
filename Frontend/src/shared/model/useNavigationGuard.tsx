60fc9b58111627eaa2037ffd9b14768f42851edf
antisdun <antiskj42@gmail.com>
2025-04-05 22:28:26 +0900

    feat: useNavigationGuard 훅에 새로고침 감지 추가

--- Frontend/src/shared/model/useNavigationGuard.tsx
+++ Frontend/src/shared/model/useNavigationGuard.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useBlocker } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useBeforeUnload, useBlocker } from 'react-router-dom';
 
 interface UseNavigationGuardProps {
     shouldBlock?: boolean;
@@ -9,9 +9,28 @@ const useNavigationGuard = ({
     shouldBlock = true,
 }: UseNavigationGuardProps = {}) => {
     const [showModal, setShowModal] = useState(false);
+
+    // 브라우저 새로고침 혹은 나가기 감지
+    useBeforeUnload((event) => {
+        event.preventDefault();
+        event.returnValue =
+            '페이지를 나가시겠습니까? 지금까지 입력한 내용은 사라집니다.';
+        return event.returnValue;
+    });
+
     const blocker = useBlocker(
-        ({ currentLocation, nextLocation }) =>
-            shouldBlock && currentLocation.pathname !== nextLocation.pathname,
+        ({ currentLocation, nextLocation, historyAction }) => {
+            // 브라우저 뒤로가기/앞으로가기 감지
+            if (historyAction === 'POP') {
+                setShowModal(true);
+                return true;
+            }
+
+            return (
+                shouldBlock &&
+                currentLocation.pathname !== nextLocation.pathname
+            );
+        },
     );
 
     useEffect(() => {
@@ -22,15 +41,17 @@ const useNavigationGuard = ({
         }
     }, [blocker]);
 
-    const confirmNavigation = useCallback(() => {
+    // 경로 이탈 확정
+    const confirmNavigation = () => {
         blocker.proceed?.();
         setShowModal(false);
-    }, [blocker]);
+    };
 
-    const cancelNavigation = useCallback(() => {
+    // 경로 이탈 취소
+    const cancelNavigation = () => {
         blocker.reset?.();
         setShowModal(false);
-    }, [blocker]);
+    };
 
     return {
         showModal,
