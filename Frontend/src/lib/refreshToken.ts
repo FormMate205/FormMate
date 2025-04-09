1f8468c275ccaaa5b825e229f98b59f0fa57a289
yiyoung <2020yyoung@gmail.com>
2025-04-09 21:13:58 +0900

    feat: FCM 오류 try catch 추가

--- Frontend/src/lib/refreshToken.ts
+++ Frontend/src/lib/refreshToken.ts
@@ -7,15 +7,20 @@ import { getDeviceToken } from './firebase';
 
 // 로그인 시 FCM 토큰 갱신
 export const refreshToken = async () => {
-    const token = await getDeviceToken();
+    try {
+        const token = await getDeviceToken();
 
-    if (!token) {
-        await deactivateDeviceToken(); // FCM 토큰이 없으면 구독 해제
-        toast.warning('알림 권한이 꺼져있어요.', {
-            description: '알림을 받으려면 브라우저에서 권한을 허용해주세요!',
-        });
-        return;
+        if (!token) {
+            await deactivateDeviceToken();
+            toast.warning('알림 권한이 꺼져있어요.', {
+                description:
+                    '알림을 받으려면 브라우저에서 권한을 허용해주세요!',
+            });
+            return;
+        }
+
+        await refreshDeviceToken(token);
+    } catch (err) {
+        console.error('FCM 토큰 갱신 실패:', err);
     }
-
-    await refreshDeviceToken(token);
 };
