d4c2cf57384468f2ff741d48a61a9d29edc45ba0
antisdun <antiskj42@gmail.com>
2025-04-09 20:51:42 +0900

    fix: 인스턴스 수정

--- Frontend/src/shared/api/instance.ts
+++ Frontend/src/shared/api/instance.ts
@@ -24,7 +24,7 @@ const publicApiPaths = [
 api.interceptors.request.use((config) => {
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken && config.headers) {
-        config.headers.Authorization = accessToken;
+        config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
 });
