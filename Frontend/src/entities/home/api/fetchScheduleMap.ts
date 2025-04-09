ed1e702f5e4ee5cc8582c5855a2fdd09b569bf3e
oille12 <oeg9176@naver.com>
2025-04-09 10:03:06 +0900

    fix: 달력 일정 조회 api 호출 구조 변경

--- Frontend/src/entities/home/api/fetchScheduleMap.ts
+++ Frontend/src/entities/home/api/fetchScheduleMap.ts
@@ -1,15 +1,41 @@
 import api from '@/shared/api/instance';
-import { ScheduleMapResponse } from '../model/types';
+import { ContractItem, ScheduleMap } from '../model/types';
 
-export const fetchScheduleMap = async (
-    viewDate: string,
-): Promise<ScheduleMapResponse> => {
-    const response = await api.get('/contract/schedule', {
-        params: {
-            now: new Date().toISOString().split('T')[0], // 오늘 날짜
-            viewDate, // ex: '2025-04-30' 보고 싶은 달의 말일
-        },
+// export const fetchScheduleMap = async (
+//     viewDate: string,
+// ): Promise<ScheduleMapResponse> => {
+//     const response = await api.get('/contract/schedule', {
+//         params: {
+//             now: new Date().toISOString().split('T')[0], // 오늘 날짜
+//             viewDate, // ex: '2025-04-30' 보고 싶은 달의 말일
+//         },
+//     });
+
+//     return response.data;
+// };
+
+export const fetchScheduleMap = async (viewDate: string) => {
+    const res = await api.get('/contract/schedule', {
+        params: { viewDate },
     });
 
-    return response.data;
+    const result: ScheduleMap = {};
+
+    res.data.forEach((item: ContractItem) => {
+        const [, , day] = item.scheduledPaymentDate;
+        const key = String(day);
+
+        if (!result[key]) {
+            result[key] = { contracts: [] };
+        }
+
+        result[key].contracts.push({
+            userIsCreditor: item.userIsCreditor,
+            contracteeName: item.contracteeName,
+            repaymentAmount: item.repaymentAmount,
+            scheduledPaymentDate: item.scheduledPaymentDate,
+        });
+    });
+
+    return result;
 };
