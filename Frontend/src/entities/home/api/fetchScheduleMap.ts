73a01b5d0d3b0a22a1c4e8c39322f1c8996d5c7e
oille12 <oeg9176@naver.com>
2025-04-09 11:14:40 +0900

    fix: 주석 제거

--- Frontend/src/entities/home/api/fetchScheduleMap.ts
+++ Frontend/src/entities/home/api/fetchScheduleMap.ts
@@ -1,19 +1,6 @@
 import api from '@/shared/api/instance';
 import { ContractItem, ScheduleMap } from '../model/types';
 
-// export const fetchScheduleMap = async (
-//     viewDate: string,
-// ): Promise<ScheduleMapResponse> => {
-//     const response = await api.get('/contract/schedule', {
-//         params: {
-//             now: new Date().toISOString().split('T')[0], // 오늘 날짜
-//             viewDate, // ex: '2025-04-30' 보고 싶은 달의 말일
-//         },
-//     });
-
-//     return response.data;
-// };
-
 export const fetchScheduleMap = async (viewDate: string) => {
     const res = await api.get('/contract/schedule', {
         params: { viewDate },
