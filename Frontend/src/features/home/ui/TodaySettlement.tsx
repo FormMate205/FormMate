d1ad9191e3cb1247437fa05c98f5ebb16d704ea4
yiyoung <2020yyoung@gmail.com>
2025-04-11 01:22:33 +0900

    fix: 알림 중복 수신 문제 해결

--- Frontend/src/features/home/ui/TodaySettlement.tsx
+++ Frontend/src/features/home/ui/TodaySettlement.tsx
@@ -20,14 +20,12 @@ const TodaySettlement = ({ data }: TodaySettlementProps) => {
                 <div className='rounded-xl bg-white px-4 py-3 text-center shadow-sm'>
                     <p className='text-line-400 mb-1 text-sm'>보낼 금액</p>
                     <p className='text-subPink-600 font-bold'>
-                        -{' '}
                         {(expectedTotalRepayment - paidAmount).toLocaleString()}
                     </p>
                 </div>
                 <div className='rounded-xl bg-white px-4 py-3 text-center shadow-sm'>
                     <p className='text-line-400 mb-1 text-sm'>받을 금액</p>
                     <p className='text-primary-500 font-bold'>
-                        +{' '}
                         {(
                             expectedTotalReceived - receivedAmount
                         ).toLocaleString()}
