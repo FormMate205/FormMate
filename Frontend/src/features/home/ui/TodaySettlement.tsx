db5839e1aec5f4913cab0626ffd0eaa341d3b808
oille12 <oeg9176@naver.com>
2025-04-10 15:06:04 +0900

    fix: 보낼, 받을 금액 계산 오류 해결

--- Frontend/src/features/home/ui/TodaySettlement.tsx
+++ Frontend/src/features/home/ui/TodaySettlement.tsx
@@ -5,7 +5,12 @@ interface TodaySettlementProps {
 }
 
 const TodaySettlement = ({ data }: TodaySettlementProps) => {
-    const { expectedTotalRepayment, expectedTotalReceived } = data!;
+    const {
+        expectedTotalRepayment,
+        expectedTotalReceived,
+        paidAmount,
+        receivedAmount,
+    } = data!;
 
     return (
         <div>
@@ -15,13 +20,17 @@ const TodaySettlement = ({ data }: TodaySettlementProps) => {
                 <div className='rounded-xl bg-white px-4 py-3 text-center shadow-sm'>
                     <p className='text-line-400 mb-1 text-sm'>보낼 금액</p>
                     <p className='text-subPink-600 font-bold'>
-                        - {expectedTotalRepayment.toLocaleString()}
+                        -{' '}
+                        {(expectedTotalRepayment - paidAmount).toLocaleString()}
                     </p>
                 </div>
                 <div className='rounded-xl bg-white px-4 py-3 text-center shadow-sm'>
                     <p className='text-line-400 mb-1 text-sm'>받을 금액</p>
                     <p className='text-primary-500 font-bold'>
-                        + {expectedTotalReceived.toLocaleString()}
+                        +{' '}
+                        {(
+                            expectedTotalReceived - receivedAmount
+                        ).toLocaleString()}
                     </p>
                 </div>
             </div>
